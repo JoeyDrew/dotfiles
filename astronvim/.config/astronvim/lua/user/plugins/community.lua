98871dec7c3f8dfb37c78f18ebeae85dc7aeade6
Joey Drew <joey@joeydrew.com>
2023-11-19 14:33:51 -0500

    add project.nvim

--- astronvim/.config/astronvim/lua/user/plugins/community.lua
+++ astronvim/.config/astronvim/lua/user/plugins/community.lua
@@ -4,4 +4,6 @@ return {
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.git.neogit" },
   { import = "astrocommunity.motion.leap-nvim"},
+  { import = "astrocommunity.project.project-nvim"},
+
 }
