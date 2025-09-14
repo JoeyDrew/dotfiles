d6b414de7f1660ed08dc72e2ed0d922a8f905eba
Joey Drew <joey@joeydrew.com>
2025-09-14 11:50:56 -0400

    remove astronvim config

--- astronvim/.config/astronvim/lua/user/mappings.lua
+++
@@ -1,6 +0,0 @@
-return {
-  -- first key is the mode
-  n = {
-    ["<leader>P"] = { "<cmd>Telescope projects<cr>", desc = "Projects"},
-  },
-}
--- astronvim/.config/astronvim/lua/user/plugins/community.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "AstroNvim/astrocommunity",
-
-  { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.motion.leap-nvim"},
-  { import = "astrocommunity.project.project-nvim"},
-}
